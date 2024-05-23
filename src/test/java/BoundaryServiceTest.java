f0644c2f31cb4d4fe181e649da44c7955c828d24
nadya <tutu87238@gmail.com>
2024-05-23 16:32:59 +0700

    билд, упавший из-за падения теста

--- src/test/java/BoundaryServiceTest.java
+++ src/test/java/BoundaryServiceTest.java
@@ -5,11 +5,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class BoundaryServiceTest {
 
     @Test
-    @DisplayName("Поиск минимального значения в массиве")
-    void testFindMin() {
+    @DisplayName("Тест, который упадет из-за падения теста")
+    void testFailingTest() {
         BoundaryService service = new BoundaryService();
         int[] marks = {5, 3, 8, 1};
         int min = service.findMin(marks);
-        assertEquals(1, min);
+        assertEquals(8, min);
     }
 }
